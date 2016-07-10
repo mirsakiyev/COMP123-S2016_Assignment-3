ff99d4f0265405493028472eae55d774562dc1ce
mirsakiyev <mirsakiyev@yahoo.com>
2016-07-09 22:16:39 -0400

    Created GiantPlanet subclass

---
+++ COMP123-S2016_Assignment-3/GiantPlanet.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace COMP123_S2016_Assignment_3
+{
+    public class GiantPlanet : Planet, IHasMoons, IHasRings
+    {
+        private string _type;
+
+        public GiantPlanet(string name, double diameter, double mass, string type)
+            :base(name,diameter,mass)
+        {
+            
+        }
+
+        public bool HasMoons()
+        {
+            if (MoonCount>0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public bool HasRings()
+        {
+            if (RingCount > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ COMP123-S2016_Assignment-3/IHasRings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace COMP123_S2016_Assignment_3
+{
+    public interface IHasRings
+    {
+    }
+}
